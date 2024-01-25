a756fd3143d005413753d05ec326a65909ec6aa9
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-01-25 12:28:08 +0530

    add image slider to the listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
 
 function App() {
   const [sidebar, setSidebar] = useState(false);
@@ -33,6 +34,7 @@ function App() {
           />
         </Route>
 
+        <Route path="/listing/:listingId" element={<Listing />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
